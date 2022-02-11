fabe000993b1ba675ff1e20d53402aba8e7643e8
Ashton George <ashtonsmail@icloud.com>
2022-02-11 10:46:16 -0500

    Initial Commit

---
+++ Hall Pass/Hall_PassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hall_PassApp.swift
+//  Hall Pass
+//
+//  Created by Ashton George on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Hall_PassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
